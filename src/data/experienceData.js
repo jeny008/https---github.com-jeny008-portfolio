8df98f19028d3804e2420d0513d898bca14964ce
jeny008 <jenytandel008@gmail.com>
2022-06-03 03:27:20 +0530

    done

---
+++ src/data/experienceData.js
@@ -0,0 +1,16 @@
+export const experienceData = [
+    {
+        id: 1,
+        company: 'Atul Ltd',
+        jobtitle: 'I  done my Internship and completed Home-Automation system project during internship',
+        startYear: '1 MAY-2019 ',
+        endYear: '1 June-2019'
+    },
+    {
+        id: 2,
+        company: 'BosLeo Technology Private Limited',
+        jobtitle: 'I  done my Internship and completed Employee management system project during internship',
+        startYear: '8 JAN-2020',
+        endYear: '8 June-2020'
+    },
+]
